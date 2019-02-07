ad1f1a78dbf233ab5dbd6a741a6113173749e572
Vladimir Vinegorov <vladko886@abv.bg>
2019-02-07 23:38:28 +0200

    add the user to the object -> removeOfflineUsers if online

--- app/middlewares/lastOnline.js
+++ app/middlewares/lastOnline.js
@@ -1,23 +1,9 @@
-let User = require('../models/User');
+let lastUsersOnline = require('../services/users/lastUsersOnline');
 let moment = require('moment');
 
 module.exports = function(req, res, next){
     if(typeof req.session.user != 'undefined'){
-        User.findOne({_id: req.session.user._id }, function(err, user){       
-            if(!err){        
-                if(moment().diff(user.lastOnline, 'minutes') > 1){ 
-
-                    user.isOnline = '0';
-                    
-                } else {
-
-                    user.isOnline = '1';
-
-                    user.lastOnline = moment().format('DD.MM.YYYY HH:mm:ss');
-                }
-                user.save();         
-            }                   
-        });         
+        lastUsersOnline[req.session.user._id] = moment().unix();
     }
     next();
 }
