9745f4636b006c8003a7cfb8e5be7ed65fe5299d
Vladimir Vinegorov <vladko886@abv.bg>
2019-02-09 00:52:12 +0200

    new changes to the feature

--- app/services/users/theUsernameExists.js
+++ app/services/users/theUsernameExists.js
@@ -1,16 +1,35 @@
 let User = require('../../models/User');
-
-module.exports = async function usernameExists(req) {
+ 
+module.exports = async function usernameExists(username){
+ 
+   let user, lastNumber, number = 0, usernameNumbers = "";
+   let newUsername = username;
+ 
+   for(let i = 0; i < username.length; i ++){
+      lastNumber = parseInt(username.substr(username.length - 1));
+ 
+      if(lastNumber){
+         usernameNumbers += lastNumber;
+         username = username.slice(0, -1);          
+      }    
+      else{
+         break;
+      }          
+   }
    
-   let user;
-   let number = 0;
-   let newUsername;
-
-   do {
-      newUsername = req.body.username + number;
-      number++;
-      user = await User.findOne({username: newUsername}).exec();    
+   if(usernameNumbers.length ){
+      usernameNumbers = parseInt((usernameNumbers.split('').reverse().join('')));
+      usernameNumbers += 1;
+      newUsername = username + usernameNumbers;
+   }
+   
+   do {    
+      user = await User.findOne({username: newUsername}).exec();
+      if(!usernameNumbers.length && user){
+         newUsername = username + number;
+         number += 1;
+      }    
    } while(user)
-
+ 
    return newUsername;
 }
